f2a769a7ae950dee8723a82f4f1ae6e487e6b354
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-18 10:28:41 -0300

    feat: criado tela da home com o componente de label

--- ArquiteturaVIPApp/Scene/Home/HomeView.swift
+++ ArquiteturaVIPApp/Scene/Home/HomeView.swift
@@ -1,8 +1,37 @@
-//
-//  HomeView.swift
-//  ArquiteturaVIPApp
-//
-//  Created by Leticia Speda on 18/01/24.
-//
+import UIKit
 
-import Foundation
+final class HomeView: UIView, ViewCode {
+    //MARK: - Properts
+    private lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Bem Vindo ao App Login VIP"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 18)
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.enableViewCode()
+        return label
+    }()
+    
+    //MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+    
+    //MARK: - Helpers
+    func setupHierarchy() {
+        addSubview(welcomeLabel)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            welcomeLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
