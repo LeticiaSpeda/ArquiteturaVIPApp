d4ff37dcaf1a40947efd4e59c56a358333feea07
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-19 11:44:35 -0300

    feat: criado calsse e protocolo para chama da api

--- ArquiteturaVIPApp/Integration/Business/UserBusiness.swift
+++ ArquiteturaVIPApp/Integration/Business/UserBusiness.swift
@@ -1,8 +1,32 @@
-//
-//  UserBusiness.swift
-//  ArquiteturaVIPApp
-//
-//  Created by Leticia Speda on 19/01/24.
-//
-
 import Foundation
+
+protocol UserBusinessProtocol {
+    func login(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void)
+    func register(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void)
+}
+
+final class UserBusiness: UserBusinessProtocol {
+    private let provider = UserProvider()
+    
+    func login(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void) {
+        provider.login(parameters: getParams( email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHadler(.success(userModel))
+            case .failure(let error):
+                completionHadler(.failure(error))
+            }
+        }
+    }
+    
+    func register(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void) {
+        provider.register(parameters: getParams(email, password)) { result in
+            switch result {
+            case .success(let userModel):
+                completionHadler(.success(userModel))
+            case .failure(let error):
+                completionHadler(.failure(error))
+            }
+        }
+    }
+}
