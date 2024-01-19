23dfa51446e112f37263fccd2615d91ef8accf53
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-19 11:43:40 -0300

    feat: criado struct com os parametros que vem da api

--- ArquiteturaVIPApp/Infraestructure/Constants.swift
+++ ArquiteturaVIPApp/Infraestructure/Constants.swift
@@ -1,8 +1,8 @@
-//
-//  Constants.swift
-//  ArquiteturaVIPApp
-//
-//  Created by Leticia Speda on 19/01/24.
-//
-
 import Foundation
+
+struct Constants {
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "usermModel"
+    }
+}
