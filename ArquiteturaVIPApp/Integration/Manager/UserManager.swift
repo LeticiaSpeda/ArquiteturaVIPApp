29e8a1c40b72df427c4a5b847444de9f2b24f058
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-19 11:47:39 -0300

    feat: criado classe manager para a chamada da api

--- ArquiteturaVIPApp/Integration/Manager/UserManager.swift
+++ ArquiteturaVIPApp/Integration/Manager/UserManager.swift
@@ -1,8 +1,37 @@
-//
-//  UserManager.swift
-//  ArquiteturaVIPApp
-//
-//  Created by Leticia Speda on 19/01/24.
-//
-
 import Foundation
+
+protocol UserManagerProtocol  {
+    func login(email: String, password: String, successHandler: @escaping(UserModel) -> Void, failureHandler: @escaping(Error) -> Void)
+    
+    func register(email: String, password: String, successHandler: @escaping(UserModel) -> Void, failureHandler: @escaping(Error) -> Void)
+}
+
+final class UserManager: UserManagerProtocol {
+    private let business: UserBusinessProtocol
+    
+    init(business: UserBusinessProtocol) {
+        self.business = business
+    }
+    
+    func login(email: String, password: String, successHandler: @escaping (UserModel) -> Void, failureHandler: @escaping (Error) -> Void) {
+        business.login(email: email, password: password) { result in
+            switch result {
+            case .success(let userModel):
+                successHandler(userModel)
+            case .failure(let error):
+                failureHandler(error)
+            }
+        }
+    }
+    
+    func register(email: String, password: String, successHandler: @escaping (UserModel) -> Void, failureHandler: @escaping (Error) -> Void) {
+        business.register(email: email, password: password) { result in
+            switch result {
+            case .success(let userModel):
+                successHandler(userModel)
+            case .failure(let error):
+                failureHandler(error)
+            }
+        }
+    }
+}
