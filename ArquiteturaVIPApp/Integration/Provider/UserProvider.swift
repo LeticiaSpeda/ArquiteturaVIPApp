dd1c529ebba8ed37be76d5b677ce15b3d565f44b
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-19 11:49:43 -0300

    feat: criado a classe do provider para chaamr a api

--- ArquiteturaVIPApp/Integration/Provider/UserProvider.swift
+++ ArquiteturaVIPApp/Integration/Provider/UserProvider.swift
@@ -1,8 +1,42 @@
-//
-//  UserProvider.swift
-//  ArquiteturaVIPApp
-//
-//  Created by Leticia Speda on 19/01/24.
-//
-
 import Foundation
+import FirebaseAuth
+
+protocol UserProviderProtocol {
+    func login(parameters: [AnyHashable: Any], completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+    
+    func register(parameters: [AnyHashable: Any], completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+}
+
+final class UserProvider: UserProviderProtocol {
+    private lazy var auth = Auth.auth()
+    
+    func login(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        guard let body = parameters[Constants.ParametersKeys.body] as? NSDictionary,
+              let userModel = body[Constants.ParametersKeys.userModel] as? UserModel else {
+            return
+        }
+        
+        self.auth.signIn(withEmail: userModel.email, password: userModel.password) { (result, error) in
+            if let error = error {
+                completionHadler(.failure(error))
+            } else {
+                completionHadler(.success(userModel))
+            }
+        }
+    }
+    
+    func register(parameters: [AnyHashable : Any], completionHadler: @escaping (Result<UserModel, Error>) -> Void) {
+        guard let body = parameters[Constants.ParametersKeys.body] as? NSDictionary,
+              let userModel = body[Constants.ParametersKeys.userModel] as? UserModel else {
+            return
+        }
+        
+        self.auth.createUser(withEmail: userModel.email, password: userModel.password) { (result, error) in
+            if let error = error {
+                completionHadler(.failure(error))
+            } else {
+                completionHadler(.success(userModel))
+            }
+        }
+    }
+}
