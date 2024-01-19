f560fa69ff091f4c2e18b65dd97e0246b156310c
LeticiaSpeda <leticiaspeda@gmail.com>
2024-01-19 12:26:01 -0300

    refactor: ajustado nome do parametro nas funcoes de login e register

--- ArquiteturaVIPApp/Integration/Business/UserBusiness.swift
+++ ArquiteturaVIPApp/Integration/Business/UserBusiness.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 protocol UserBusinessProtocol {
-    func login(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void)
-    func register(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void)
+    func login(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void)
+    func register(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void)
 }
 
 final class UserBusiness: UserBusinessProtocol {
     private let provider = UserProvider()
     
-    func login(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void) {
+    func login(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void) {
         provider.login(parameters: getParams( email, password)) { result in
             switch result {
             case .success(let userModel):
@@ -19,7 +19,7 @@ final class UserBusiness: UserBusinessProtocol {
         }
     }
     
-    func register(email: String, password: String, completion: @escaping(Result<UserModel, Error>) -> Void) {
+    func register(email: String, password: String, completionHadler: @escaping(Result<UserModel, Error>) -> Void) {
         provider.register(parameters: getParams(email, password)) { result in
             switch result {
             case .success(let userModel):
@@ -28,5 +28,11 @@ final class UserBusiness: UserBusinessProtocol {
                 completionHadler(.failure(error))
             }
         }
+    }
+    
+    private func getParams(_ email: String, _ password: String) -> [AnyHashable: Any] {
+        let userModel = UserModel(email: email, password: password)
+        let params: [AnyHashable: Any] = [Constants.ParametersKeys.body: [Constants.ParametersKeys.userModel: userModel]]
+        return params
     }
 }
