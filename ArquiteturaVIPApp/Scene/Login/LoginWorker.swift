35bd2cb3897ed8b7d59f2e4a79e5011906b02acc
Leticia <leticiaspeda@gmail.com>
2024-01-22 16:45:24 -0300

    feat: criado funcao para o login

--- ArquiteturaVIPApp/Scene/Login/LoginWorker.swift
+++ ArquiteturaVIPApp/Scene/Login/LoginWorker.swift
@@ -2,6 +2,14 @@ import UIKit
 
 class LoginWorker {
     
-    func loginUser() {
+    func loginUser(userModel: UserModel, successHandler: @escaping (UserModel?) -> Void, failureHandler: @escaping (Error?) -> Void) {
+        
+        let manager = UserManager(business: UserBusiness())
+        manager.login(email: userModel.email, password: userModel.password) { userModel in
+            successHandler(userModel)
+        } failureHandler: { error in
+            failureHandler(error)
+        }
+
   }
 }
