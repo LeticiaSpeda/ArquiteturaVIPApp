77810a4f0436fdbd1003bf516f50b294ee8c0c9a
Leticia <leticiaspeda@gmail.com>
2024-01-22 16:38:34 -0300

    feat: chamada do response e erro

--- ArquiteturaVIPApp/Scene/Login/LoginInteractor.swift
+++ ArquiteturaVIPApp/Scene/Login/LoginInteractor.swift
@@ -13,9 +13,19 @@ class LoginInteractor: LoginBusinessLogic, LoginDataStore {
   // MARK: Do something
   func doSomething(request: Login.Something.Request) {
     worker = LoginWorker()
-    worker?.loginUser()
-    
-    let response = Login.Something.Response()
-    presenter?.presentSomething(response: response)
+      worker?.loginUser(userModel: request.userModel, successHandler: { [weak self] userModel in
+          
+          if let self {
+              let response = Login.Something.Response(userModel: userModel ?? UserModel())
+              self.presenter?.presentSomething(response: response)
+          }
+          
+      }, failureHandler: { [weak self] error in
+          if let self, let error = error, let viewError = Login.Something.ViewError(error: error) {
+              self.presenter?.presentError(error: error)
+              }
+          }
+                        
+      )}
   }
-}
+
