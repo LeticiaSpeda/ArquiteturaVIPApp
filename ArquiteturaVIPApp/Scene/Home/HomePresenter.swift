677686a5efbdb4e7ac6c7dfd253d3946fd7b266d
Leticia <leticiaspeda@gmail.com>
2024-01-23 13:26:02 -0300

    refactor: retirado codigo que pertencia ao loginPresenter

--- ArquiteturaVIPApp/Scene/Home/HomePresenter.swift
+++ ArquiteturaVIPApp/Scene/Home/HomePresenter.swift
@@ -2,20 +2,15 @@ import UIKit
 
 protocol HomePresentationLogic {
     func presentSomething(response: Home.Something.Response)
-    func presentError(error: Login.Something.ViewError)
 }
 
 class HomePresenter: HomePresentationLogic {
-   
+    
     weak var viewController: HomeDisplayLogic?
     
     // MARK: Do something
     func presentSomething(response: Home.Something.Response) {
         let viewModel = Home.Something.ViewModel()
         viewController?.displaySomething(viewModel: viewModel)
-    }
-    func presentError(error: Login.Something.ViewError) {
-        let viewError = Login.Something.ViewError(error: error)
-        viewController?.displayError(error: viewError)
     }
 }
